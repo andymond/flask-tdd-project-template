2b6426efacd6ce618a66535d4a9c04e23633625e
Andrew Dymond <andy.dymond@gmail.com>
2020-03-15 17:32:23 -0600

    Add get sample item by id endpoint

--- project/api/sample_items/views.py
+++ project/api/sample_items/views.py
@@ -18,7 +18,16 @@ sample_item_serializer = sample_items_namespace.model(
 class SampleItems(Resource):
     @sample_items_namespace.marshal_with(sample_item_serializer, as_list=True)
     def get(self):
-        return db_service.all(SampleItem)
+        return db_service.all(SampleItem), 200
+
+class SampleItemById(Resource):
+    @sample_items_namespace.marshal_with(sample_item_serializer)
+    def get(self, siid):
+        sample_item = db_service.find(SampleItem, siid)
+        if not sample_item:
+            sample_items_namespace.abort(404, f"Sample Item #{siid} not found")
+        return sample_item, 200
 
 
 sample_items_namespace.add_resource(SampleItems, "")
+sample_items_namespace.add_resource(SampleItemById, "/<int:siid>")
--- project/tests/sample_items/test_views.py
+++ project/tests/sample_items/test_views.py
@@ -15,3 +15,15 @@ def test_get_all_sample_items(test_app, test_database):
     assert data[0]["name"] == items[0].name
     assert data[1]["name"] == items[1].name
     assert data[2]["name"] == items[2].name
+
+def test_get_single_sample_item(test_app, test_database):
+    item = db_service.create(SampleItem, name= "item1")
+    client = test_app.test_client()
+    resp = client.get(f"/sample_items/{item.id}")
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 200
+    assert data["name"] == item.name
+
+    resp = client.get(f"/sample_items/0")
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 404
