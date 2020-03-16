f54da90a04ba7dbe845b84ffb41df11495ae9a4b
Andrew Dymond <andy.dymond@gmail.com>
2020-03-16 17:16:55 -0600

    format login form

--- project/api/admin_users/login_form.py
+++ project/api/admin_users/login_form.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, PasswordField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
-    name = StringField('name', validators=[DataRequired()])
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
